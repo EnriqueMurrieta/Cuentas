280c2aa8cbaaaf7109b363138915e875212587de
Enrique Murrieta <enrique_murrieta@hotmail.com>
2021-04-22 22:21:01 -0700

    Working on data structure/flow to send to db

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar, Image, FlatList, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar, Image, TextInput, TouchableOpacity, Button } from 'react-native';
 
 import firebase from 'firebase';
 
-export default class Home extends Component {
+export default function Home({navigation, NewT="NewT"}) {
+
+    const [name, onChangeName] = React.useState("");
+    const [project, onChangeProject] = React.useState(false);
+
+
 
     logout = () => {
         firebase.auth().signOut().then(() => {
@@ -11,15 +16,52 @@ export default class Home extends Component {
         })
     }
 
-    render(){
-        return(
-            <View>
+    return(
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
                 <TouchableOpacity>
-                    <Button title="Nuevo proyecto" onPress={() => this.props.navigation.navigate('NewT')}/>
+                    <Button title="Nuevo Proyecto" onPress={() => onChangeProject(!project)}/>
                 </TouchableOpacity>
-                <Text></Text>
+                {project == true ?
+                    <View style={styles.form}>
+                        <TextInput value={name} onChangeText={(entry) => onChangeName(entry)} placeholder="Nombre del proyecto" style={styles.input}/>
+                        {name != "" ?
+                            <Button title="Ir" onPress={() => navigation.navigate(NewT, {name:name})} style={styles.submit}/>
+                        : null }
+                    </View>
+                : null } 
+                <Text>LALALALA</Text>
                 <Button title="Sign Out" onPress={() => firebase.auth().signOut()}/>
-            </View>
-        );
-    }
+            </ScrollView>
+        </SafeAreaView>
+        
+    );
 }
+
+/*
+ <Button title="Nuevo proyecto" onPress={() => navigation.navigate(NewT)}/>
+*/
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    form: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        padding: 15
+    },
+    input: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+        textAlign: 'center',
+        fontSize: 25,
+        width: '80%'
+    },
+    submit: {
+        width: '20%'
+    }
+})
--- src/containers/NewT.js
+++ src/containers/NewT.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, StatusBar, Image, FlatList, TouchableOpacity, Button } from 'react-native';
+import { roundToNearestPixel } from 'react-native/Libraries/Utilities/PixelRatio';
 import Test from './Test';
 
-export default class Home extends Component {
-    render(){
-        return(
-            <ScrollView>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-                <Test/>
-            </ScrollView>
-        );
+export default function Home ({route, navigation}) {
+
+    const [number, onChangeNumber] = React.useState({});
+    const [title, onChangeTitle] = React.useState("");
+    const [total, onChangeTotal] = React.useState();
+
+    const eventHandler = data => {
+
     }
+    const {name} = route.params;
+    return(
+        <ScrollView>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+        </ScrollView>
+    );
 }
